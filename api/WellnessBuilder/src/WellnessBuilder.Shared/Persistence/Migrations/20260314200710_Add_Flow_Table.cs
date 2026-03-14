7e5e777b2a90fab3e0d0504339042e898b11dbf3
Kliost <yurii.kliost@gmail.com>
2026-03-14 22:07:49 +0200

    [migration]: Add flow table

---
+++ api/WellnessBuilder/src/WellnessBuilder.Shared/Persistence/Migrations/20260314200710_Add_Flow_Table.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WellnessBuilder.Shared.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Flow_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "FlowId",
+                table: "Offers",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FlowId",
+                table: "Nodes",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FlowId",
+                table: "Edges",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Flow",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flow", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flow");
+
+            migrationBuilder.DropColumn(
+                name: "FlowId",
+                table: "Offers");
+
+            migrationBuilder.DropColumn(
+                name: "FlowId",
+                table: "Nodes");
+
+            migrationBuilder.DropColumn(
+                name: "FlowId",
+                table: "Edges");
+        }
+    }
+}
