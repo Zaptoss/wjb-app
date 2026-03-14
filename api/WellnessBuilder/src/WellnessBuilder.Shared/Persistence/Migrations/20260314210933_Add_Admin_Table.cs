3d4e8f51181d54c8222562b1f09599df1f306947
Kliost <yurii.kliost@gmail.com>
2026-03-14 23:27:05 +0200

    [migration]: add admin table

---
+++ api/WellnessBuilder/src/WellnessBuilder.Shared/Persistence/Migrations/20260314210933_Add_Admin_Table.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WellnessBuilder.Shared.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Admin_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+    }
+}
