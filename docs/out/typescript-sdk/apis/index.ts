a22a53863705d422a43bb3564eea0e75e0d7a173
Zaptoss <zaptosgg@gmail.com>
2026-03-15 01:41:21 +0200

    Remove codegen

--- docs/out/typescript-sdk/apis/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ConditionGroupsApi';
-export * from './ConditionsApi';
-export * from './EdgesApi';
-export * from './FlowsApi';
-export * from './NodesApi';
-export * from './OffersApi';
-export * from './OptionsApi';
