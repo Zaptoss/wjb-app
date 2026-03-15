a8d35432dae46ca0438763e295064ce97e92dff3
Kliost <yurii.kliost@gmail.com>
2026-03-15 04:57:22 +0200

    [feat]: add flow get all pagination

---
+++ api/WellnessBuilder/src/WellnessBuilder.Shared/Contracts/Common/PagedRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace WellnessBuilder.Shared.Contracts.Common;
+
+public class PagedRequest
+{
+    private int _pageSize = 10;
+    private int _page = 1;
+
+    public int Page
+    {
+        get => _page;
+        set => _page = value < 1 ? 1 : value;
+    }
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value < 1 ? 1 : value > 50 ? 50 : value;
+    }
+}
